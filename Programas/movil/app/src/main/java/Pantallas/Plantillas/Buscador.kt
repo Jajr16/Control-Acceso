0d2e86b306614fd19cf76aad3a31513b661364e5
Alfredo Jiménez <jajr0316@gmail.com>
2025-02-27 21:49:36 -0600

    Plantillas

---
+++ Programas/movil/app/src/main/java/Pantallas/Plantillas/Buscador.kt
@@ -0,0 +1,30 @@
+package Pantallas.Plantillas
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Buscador(searchQuery: String, onSearchQueryChanged: (String) -> Unit, placeholder: String) {
+    OutlinedTextField(
+        value = searchQuery,
+        onValueChange = { onSearchQueryChanged(it) },
+        placeholder = { Text(placeholder) },
+        leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Buscar") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color.LightGray, shape = MaterialTheme.shapes.medium)
+    )
+}
--- Programas/movil/app/src/main/java/Pantallas/WelcomeScreenAlumno.kt
+++ Programas/movil/app/src/main/java/Pantallas/WelcomeScreenAlumno.kt
@@ -1,5 +1,6 @@
 package Pantallas
 
+import Pantallas.Plantillas.WelcomeScreenBase
 import Pantallas.components.MenuBottomBar
 import Pantallas.components.ValidateSession
 import android.content.Context
@@ -48,116 +49,145 @@ import com.example.prueba3.Views.LoginViewModel
 import com.example.prueba3.ui.theme.BlueBackground
 
 @Composable
-fun WelcomeScreenAlumno(navController: NavController,
-                           loginViewModel: LoginViewModel,
-                           viewModel: HomeViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),) {
+fun WelcomeScreenAlumno(navController: NavController, loginViewModel: LoginViewModel){
+    WelcomeScreenBase(navController, loginViewModel, "Bienvenido Alumno") {
+        Spacer(modifier = Modifier.height(50.dp))
 
-    val userRole = loginViewModel.getUserRole()
-    val username = loginViewModel.getUserName()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OptionButton(
+                title = "Listado de ETS",
+                icon = ImageVector.vectorResource(id = R.drawable.exam),
+                onClick = { navController.navigate("LETSA") },
+                modifier = Modifier.size(150.dp)
+            )
 
-    val confirmation by remember { viewModel.StatusInscripcion }.collectAsState()
+            Spacer(modifier = Modifier.width(20.dp))
 
-    LaunchedEffect(username) {
-        username?.let {
-            viewModel.getConfirmationInscription(it)
+            OptionButton(
+                title = "Información de acceso",
+                icon = Icons.Default.Info,
+                onClick = { navController.navigate("info") },
+                modifier = Modifier.size(150.dp)
+            )
         }
     }
 
-    Scaffold(
-        bottomBar = { MenuBottomBar(navController = navController, userRole) }
-    ) { padding ->
-        androidx.compose.foundation.layout.Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(BlueBackground)
-                .padding(padding)
-        )
-
-        ValidateSession (navController = navController) {
-            // Contenedor principal centrado
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 100.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                Image(
-                    painter = painterResource(id = R.drawable.escom),
-                    contentDescription = "Logo IPN",
-                    modifier = Modifier
-                        .size(250.dp)
-                )
-
-                Spacer(modifier = Modifier.height(15.dp))
-
-                Text(
-                    text = "Bienvenido Usuario.",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(bottom = 16.dp),
-                    color = Color.White,
-                )
-
-                Divider(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .width(200.dp),
-                    thickness = 1.dp,
-                    color = Color.White
-                )
-
-                when (confirmation) {
-                    true -> {
-                        Text(
-                            text = "¿Qué deseas hacer?",
-                            style = MaterialTheme.typography.bodyMedium,
-                            modifier = Modifier.padding(bottom = 32.dp),
-                            color = Color.White,
-                        )
-
-                        Spacer(modifier = Modifier.height(50.dp))
-
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            OptionButton(
-                                title = "ETS Inscritos",
-                                icon = ImageVector.vectorResource(id = R.drawable.exam),
-                                onClick = { navController.navigate("LETSA") },
-                                modifier = Modifier.size(150.dp)
-                            )
-
-                            Spacer(modifier = Modifier.width(20.dp))
-
-                            OptionButton(
-                                title = "Información de acceso",
-                                icon = Icons.Default.Info,
-                                onClick = { navController.navigate("info") },
-                                modifier = Modifier.size(150.dp),
-                            )
-                        }
-                    }
-                    false -> {
-                        Text(
-                            text = "No es tiempo de ETS",
-                            style = MaterialTheme.typography.bodyMedium,
-                            modifier = Modifier.padding(bottom = 32.dp),
-                            color = Color.White,
-                        )
-                    }
-                    null -> {
-                        Text(
-                            text = "Cargando...",
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = Color.White,
-                        )
-                    }
-                }
-            }
-        }
-    }
 }
+
+//@Composable
+//fun WelcomeScreenAlumno(navController: NavController,
+//                           loginViewModel: LoginViewModel,
+//                           viewModel: HomeViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),) {
+//
+//    val userRole = loginViewModel.getUserRole()
+//    val username = loginViewModel.getUserName()
+//
+//    val confirmation by remember { viewModel.StatusInscripcion }.collectAsState()
+//
+//    LaunchedEffect(username) {
+//        username?.let {
+//            viewModel.getConfirmationInscription(it)
+//        }
+//    }
+//
+//    Scaffold(
+//        bottomBar = { MenuBottomBar(navController = navController, userRole) }
+//    ) { padding ->
+//        androidx.compose.foundation.layout.Box(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(BlueBackground)
+//                .padding(padding)
+//        )
+//
+//        ValidateSession (navController = navController) {
+//            // Contenedor principal centrado
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .padding(top = 50.dp),
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//
+//                Image(
+//                    painter = painterResource(id = R.drawable.escom),
+//                    contentDescription = "Logo IPN",
+//                    modifier = Modifier
+//                        .size(250.dp)
+//                )
+//
+//                Spacer(modifier = Modifier.height(15.dp))
+//
+//                Text(
+//                    text = "Bienvenido Usuario.",
+//                    style = MaterialTheme.typography.titleLarge,
+//                    modifier = Modifier.padding(bottom = 16.dp),
+//                    color = Color.White,
+//                )
+//
+//                Divider(
+//                    modifier = Modifier
+//                        .padding(vertical = 8.dp)
+//                        .width(200.dp),
+//                    thickness = 1.dp,
+//                    color = Color.White
+//                )
+//
+//                when (confirmation) {
+//                    true -> {
+//                        Text(
+//                            text = "¿Qué deseas hacer?",
+//                            style = MaterialTheme.typography.bodyMedium,
+//                            modifier = Modifier.padding(bottom = 32.dp),
+//                            color = Color.White,
+//                        )
+//
+//                        Spacer(modifier = Modifier.height(50.dp))
+//
+//                        Row(
+//                            modifier = Modifier.fillMaxWidth(),
+//                            horizontalArrangement = Arrangement.Center
+//                        ) {
+//                            OptionButton(
+//                                title = "Mis ETS",
+//                                icon = ImageVector.vectorResource(id = R.drawable.exam),
+//                                onClick = { navController.navigate("LETSA") },
+//                                modifier = Modifier.size(150.dp)
+//                            )
+//
+//                            Spacer(modifier = Modifier.width(20.dp))
+//
+//                            OptionButton(
+//                                title = "Información de acceso",
+//                                icon = Icons.Default.Info,
+//                                onClick = { navController.navigate("info") },
+//                                modifier = Modifier.size(150.dp),
+//                            )
+//                        }
+//                    }
+//                    false -> {
+//                        Text(
+//                            text = "No es tiempo de ETS",
+//                            style = MaterialTheme.typography.bodyMedium,
+//                            modifier = Modifier.padding(bottom = 32.dp),
+//                            color = Color.White,
+//                        )
+//                    }
+//                    null -> {
+//                        Text(
+//                            text = "Cargando...",
+//                            style = MaterialTheme.typography.bodyMedium,
+//                            color = Color.White,
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 @Composable
 fun OptionButton(
