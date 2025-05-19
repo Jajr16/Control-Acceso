ecaf3325a4b8c62a16fb14788f471f10ec6bfef2
Alfredo Jiménez <jajr0316@gmail.com>
2025-05-18 23:27:45 -0600

    Corrección de periodos y calendario de app

--- Programas/movil/app/src/main/java/RetroFit/RetrofitInstance.kt
+++ Programas/movil/app/src/main/java/RetroFit/RetrofitInstance.kt
@@ -9,7 +9,7 @@
     import java.util.concurrent.TimeUnit
     
     object RetrofitInstance {
-//        private const val BASE_URL = "http://192.168.1.73:8080/"
+//        private const val BASE_URL = "http://192.168.1.68:8080/"
         private const val BASE_URL = "https://serverspringboot-asceeudmhackgbfr.mexicocentral-01.azurewebsites.net"
 
     private val okHttpClient = OkHttpClient.Builder()
